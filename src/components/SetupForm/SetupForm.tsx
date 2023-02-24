2a25b058a5f99bd2e09d5584f326f6328550cabe
Seb Scothorn <seb.scothorn@bjss.com>
2023-02-24 18:18:08 +0000

    Added Players and Subs sections

--- src/components/SetupForm/SetupForm.tsx
+++ src/components/SetupForm/SetupForm.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { Player } from '../Player/Player';
 import PlayerForm from '../Player/PlayerForm';
+import PlayerBox from '../Player/PlayerBox';
 
 export default function SetupForm() {
-	const testDefaultPlayers: Player[] = [{ name: 'Seb' }, { name: 'Dec' }];
+	const testDefaultPlayers: Player[] = [{ name: 'Seb' }, { name: 'Dec' }, { name: 'Jake' }, { name: 'Charlie' }];
 	const [teamSize, setTeamSize] = useState(5);
 	const [players, setPlayers] = useState<Player[]>(testDefaultPlayers);
 	const [addingPlayer, setAddingPlayer] = useState(false);
@@ -24,9 +25,12 @@ export default function SetupForm() {
 		setAddingPlayer(false);
 	}
 
-	const playersList = players.map((player, i) => {
-		// console.log(player);
-		return <div key={i}>{player.name}</div>;
+	const playersList = players.slice(0, teamSize * 2).map((player, i) => {
+		return <PlayerBox player={player} key={i} />;
+	});
+
+	const subsList = players.slice(teamSize * 2).map((player, i) => {
+		return <PlayerBox player={player} key={i} />;
 	});
 
 	return (
@@ -35,7 +39,10 @@ export default function SetupForm() {
 				<label htmlFor="team-size">Max Team Size</label>
 				<input type="number" name="team-size" value={teamSize} min="1" max="11" onChange={onTeamSizeChanged} />
 			</form>
+			<h3>Players</h3>
 			{playersList}
+			{subsList.length > 0 && <h3>Subs</h3>}
+			{subsList}
 			{addingPlayer ? (
 				<PlayerForm onSubmit={onNewPlayerAdded} onCancel={onNewPlayerCancelled} />
 			) : (
